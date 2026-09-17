fea96997fdc5bd19b3f7451c0e29120d8e05b419
ViajerosRadio <viajerosdelfolklore@hotmail.com>
2026-09-17 16:07:49 -0300

    Fix: force production mode and auth baseURL

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // server.js - Entry point para cPanel Node.js (Phusion Passenger)
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+process.env.NODE_ENV = 'production';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const next = require('next');
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  const http = require('http');
+  const { parse } = require('url');
+  http.createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err;
+  }).listen(process.env.PORT || 3000, () => {
     console.log('> Ready on port ' + (process.env.PORT || 3000));
   });
 });
